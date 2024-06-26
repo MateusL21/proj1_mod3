a4f3a39ac915531b6d55d535b7a37681d0645c73
MateusL21 <mateusleonel27@gmail.com>
2024-06-26 20:33:02 -0300

    first commit

---
+++ src/main/java/com/abutua/model/projectmod3/Projectmod3Application.java
@@ -0,0 +1,13 @@
+package com.abutua.model.projectmod3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectmod3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectmod3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abutua/model/projectmod3/Projectmod3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abutua.model.projectmod3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectmod3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
